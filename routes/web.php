605ac5a8b3b9e998a56523c4ac2e71e952a86c30
rofail.flouta.mohep@gmail.com <moheb-rofail@moheb-rofail.(none)>
2024-09-18 17:44:36 +0300

    Auth by middleware

--- routes/web.php
+++ routes/web.php
@@ -5,15 +5,18 @@ use Illuminate\Support\Facades\Route;
 // Note that the word App starts with capital A!
 use App\Http\Controllers\PostsController;
 
-Route::get('/', [PostsController::class, 'index'])->name('posts.index');
-Route::get('/posts/create', [PostsController::class, 'create'])->name('posts.create');
-Route::post('/posts/store', [PostsController::class, 'store'])->name('posts.store');
-Route::get('/posts/{post}/edit', [PostsController::class, 'edit'])->name('posts.edit');
-Route::put('/posts/{post}', [PostsController::class, 'update'])->name('posts.update');
-Route::delete('/posts/{post}', [PostsController::class, 'destroy'])->name('posts.destroy');
-Route::get('/posts/{post}', [PostsController::class, 'show'])->name('posts.show');
-
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Middleware here is to verify logging in before doing anything
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', [PostsController::class, 'index'])->name('posts.index');
+    Route::get('/posts/create', [PostsController::class, 'create'])->name('posts.create');
+    Route::post('/posts/store', [PostsController::class, 'store'])->name('posts.store');
+    Route::get('/posts/{post}/edit', [PostsController::class, 'edit'])->name('posts.edit');
+    Route::put('/posts/{post}', [PostsController::class, 'update'])->name('posts.update');
+    Route::delete('/posts/{post}', [PostsController::class, 'destroy'])->name('posts.destroy');
+    Route::get('/posts/{post}', [PostsController::class, 'show'])->name('posts.show');
+});
